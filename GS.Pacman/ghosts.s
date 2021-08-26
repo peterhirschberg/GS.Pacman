1a9c42d09c4ea4480ef885deac9ed4dd8603159d
Peter Hirschberg <peter@Peters-iMac.local>
2021-08-26 06:01:54 -0400

    Saving changes

---
+++ GS.Pacman/ghosts.s
@@ -0,0 +1,21 @@
+;
+;  ghosts.s
+;  GS.Pacman
+;
+;  Created by Peter Hirschberg on 8/26/21.
+;Copyright © 2021 Peter Hirschberg. All rights reserved.
+;
+
+        case on
+
+
+ghosts start
+        using globalData
+        using ghostData
+        
+
+        end
+
+ghostData data
+    
+        end
---
+++ GS.Pacman/pac.s
@@ -0,0 +1,21 @@
+;
+;  pac.s
+;  GS.Pacman
+;
+;  Created by Peter Hirschberg on 8/26/21.
+;Copyright © 2021 Peter Hirschberg. All rights reserved.
+;
+
+        case on
+
+
+pac start
+        using globalData
+        using pacData
+    
+    
+        end
+
+pacData data
+    
+        end
